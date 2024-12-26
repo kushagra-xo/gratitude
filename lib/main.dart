d82cc10b2deb45c2d0199d6b9426274283a40760
Kushagra Jain <volodarolathana@tutanota.com>
2024-12-26 23:57:18 +0530

    refactor:(comments)

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   /*
-     Everything in flutter is a widget, and your application is a
+     everything in flutter is a widget, and your application is a
      tree of these widgets, and MyApp in this case acts as the root
      of this widget tree.
    */
@@ -24,20 +24,35 @@ class MyApp extends StatelessWidget {
       home: const MyHomePage(title: 'Your Gratitude'),
     );
   }
+  // mostly has meta-data such as the title, type of theme and so on
 }
 
 class MyHomePage extends StatefulWidget {
+
+  /*
+  this right here is a stateful widget, i.e. a dynamic widget
+  which basically means it has the ability to change itself given certain
+  changes in state.
+
+  in more easy terms, it can look out for change, and change itself accordingly.
+   */
+
   const MyHomePage({super.key, required this.title});
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+   /*
+   every stateful widget has two parts,
+   1. an immutable widget, the widget itself
+   2. a mutable state widget, to hold changing data
+    */
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( // provides basic structure for the screen
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
